792e2a514dfbc5ded897c535b324f31e8e45c94f
Anne Chau <nhan.nguyen@ebuynow.com>
2023-10-17 09:28:17 +0700

    update validation pipe

---
+++ src/tasks/pipe/task-status-validation-pipe.ts
@@ -0,0 +1,29 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { TaskStatus } from "../task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatused = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE
+    ];
+
+    transform(value: any, metadata: ArgumentMetadata) {
+        value = value.toUpperCase();
+
+        //console.log('value', value);
+        //console.log('metadata', metadata);
+
+        if( !this.isStatusValid(value)){
+            throw new BadRequestException(`${value} is an invalid status`);
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any){
+       const idx = this.allowedStatused.indexOf(status);
+       return idx !== -1;
+    }
+
+}
