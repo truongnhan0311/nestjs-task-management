8e7daae420ce9f5c09925b861221bdc36595dd76
Anne Chau <nhan.nguyen@ebuynow.com>
2023-10-17 10:44:11 +0700

    type orm

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5434,
+    username: 'dataflow',
+    password: 'DataFlow2023!',
+    database: 'dataflow',
+    entities: [__dirname + '/../**/*.entity.ts'],
+    synchronize: true
+}
